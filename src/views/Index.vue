fd892e639c49a7904e89f6c1f726a0a86fc4bb42
here_for_you <houjunjie@eastbabel.cn>
2022-10-05 17:13:53 +0800

    项目初始化

---
+++ src/views/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="index-container">
+    <el-button type="success">1</el-button>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.index-container {
+}
+</style>
